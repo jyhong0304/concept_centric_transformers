8f49d82fcb3ca54480b416fe5b4ed6a41770a903
Jinyung Hong <43322245+jyhong0304@users.noreply.github.com>
2023-05-23 10:55:17 +0900

    Change the name of output text file.

--- scripts/MNIST_OddEven/slotctc_epochs10.py
+++ scripts/MNIST_OddEven/slotctc_epochs10.py
@@ -28,7 +28,7 @@ def run_exp(gpu_num, in_queue):
         print(f"==> Starting experiment {kwargs_to_cmd(experiment)}")
         os.system(kwargs_to_cmd(experiment))
 
-        with open("output.txt", "a+") as f:
+        with open("slotctc_epochs10_output.txt", "a+") as f:
             f.write(
                 f"Finished experiment {experiment} in {str((time.time() - before) / 60.0)}."
             )
